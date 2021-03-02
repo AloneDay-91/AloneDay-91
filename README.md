e4f6ff03e0f71c0f4bb31541f303037d914b64c7
AloneDay <56214457+AloneDay-91@users.noreply.github.com>
2021-03-02 20:50:18 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,16 +1,70 @@
-### Hi there 👋
+# spotify-github-profile
 
-<!--
-**AloneDay-91/AloneDay-91** is a ✨ _special_ ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+Create Spotify now playing card on your github profile
 
-Here are some ideas to get you started:
+Running on Vercel serverless function, store data in Firebase (store only access_token, refresh_token, token_expired_timestamp)
 
-- 🔭 I’m currently working on ...
-- 🌱 I’m currently learning ...
-- 👯 I’m looking to collaborate on ...
-- 🤔 I’m looking for help with ...
-- 💬 Ask me about ...
-- 📫 How to reach me: ...
-- 😄 Pronouns: ...
-- ⚡ Fun fact: ...
--->
+## Connect & Grant Permission
+
+- Click `Connect with Spotify` button below to grant permission
+
+[<img src="/img/btn-spotify.png">](https://spotify-github-profile.vercel.app/api/login)
+
+## Example
+
+![spotify-github-profile](/img/example.svg)
+
+## Running for development locally
+
+To develop locally, you need:
+
+- A fork of this project as your repository
+- A Vercel project connected with the forked repository
+- A Firebase project with Cloud Firestore setup
+- A Spotify developer account
+
+### Setting up Vercel
+
+- [Create a new Vercel project by importing](https://vercel.com/import) the forked project on GitHub
+
+### Setting up Firebase
+
+- Create [a new Firebase project](https://console.firebase.google.com/u/0/)
+- Create a new Cloud Firestore in the project
+- Download configuration JSON file from _Project settings_ > _Service accounts_ > _Generate new private key_
+- Convert private key content as BASE64
+  - You can use Encode/Decode extension in VSCode to do so
+  - This key will be used in step explained below
+
+### Setting up Spotify dev
+
+- Login to [developer.spotify.com](https://developer.spotify.com/dashboard/applications)
+- Create a new project
+- Edit settings to add _Redirect URIs_
+  - add `http://localhost:3000/api/callback`
+
+### Running locally
+
+- Install [Vercel command line](https://vercel.com/download) with `npm i -g vercel`
+- Create `.env` file at the root of the project and paste your keys in `SPOTIFY_CLIENT_ID`, `SPOTIFY_SECRET_ID`, and `FIREBASE`
+
+```sh
+BASE_URL='http://localhost:3000/api'
+SPOTIFY_CLIENT_ID='____'
+SPOTIFY_SECRET_ID='____'
+FIREBASE='__BASE64_FIREBASE_JSON_FILE__'
+```
+
+- Run `vercel dev`
+
+```sh
+$ vercel dev
+Vercel CLI 20.1.2 dev (beta) — https://vercel.com/feedback
+> Ready! Available at http://localhost:3000
+```
+
+- Now try to access http://localhost:3000/api/login
+
+## Credit
+
+Inspired by https://github.com/natemoo-re
---
+++ api/view.py
@@ -0,0 +1,236 @@
+from flask import Flask, Response, jsonify, render_template, redirect, request
+from base64 import b64decode, b64encode
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+from firebase_admin import credentials
+from firebase_admin import firestore
+from sys import getsizeof
+import firebase_admin
+
+from time import time
+
+import os
+import json
+from util import spotify
+import random
+import requests
+import functools
+
+print("Starting Server")
+
+firebase_config = os.getenv("FIREBASE")
+firebase_dict = json.loads(b64decode(firebase_config))
+
+cred = credentials.Certificate(firebase_dict)
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+CACHE_TOKEN_INFO = {}
+
+app = Flask(__name__)
+
+
+@functools.lru_cache(maxsize=128)
+def generate_css_bar(num_bar=75):
+    css_bar = ""
+    left = 1
+    for i in range(1, num_bar + 1):
+
+        anim = random.randint(350, 500)
+        css_bar += ".bar:nth-child({})  {{ left: {}px; animation-duration: {}ms; }}".format(
+            i, left, anim
+        )
+        left += 4
+
+    return css_bar
+
+
+@functools.lru_cache(maxsize=128)
+def load_image_b64(url):
+
+    resposne = requests.get(url)
+    return b64encode(resposne.content).decode("ascii")
+
+
+@functools.lru_cache(maxsize=128)
+def make_svg(artist_name, song_name, img, is_now_playing, cover_image, theme):
+
+    print("make_svg")
+
+    height = 0
+    num_bar = 75
+
+    if theme == 'compact':
+      if cover_image:
+        height = 400
+      else:
+        height = 100
+    elif theme == 'natemoo-re':
+        height = 84
+        num_bar = 100
+    elif theme == 'novatorem':
+        height = 100
+        num_bar = 100
+    else:
+      if cover_image:
+        height = 445
+      else:
+        height = 145
+
+    if is_now_playing:
+        title_text = "Now playing"
+        content_bar = "".join(["<div class='bar'></div>" for i in range(num_bar)])
+    else:
+        title_text = "Recently played"
+        content_bar = ""
+
+    css_bar = generate_css_bar(num_bar)
+
+    rendered_data = {
+        "height": height,
+        "num_bar": num_bar,
+        "content_bar": content_bar,
+        "css_bar": css_bar,
+        "title_text": title_text,
+        "artist_name": artist_name,
+        "song_name": song_name,
+        "img": img,
+        "cover_image": cover_image,
+    }
+
+    if theme != 'default':
+      return render_template(f"spotify.{theme}.html.j2", **rendered_data)
+    else:
+      return render_template("spotify.html.j2", **rendered_data)
+
+
+def get_cache_token_info(uid):
+    global CACHE_TOKEN_INFO
+
+    token_info = CACHE_TOKEN_INFO.get(uid, None)
+
+    if type(token_info) == dict:
+        current_ts = int(time())
+        expired_ts = token_info.get("expired_ts")
+        if expired_ts is None or current_ts >= expired_ts:
+            return None
+
+    return token_info
+
+
+def get_access_token(uid):
+    global CACHE_TOKEN_INFO
+
+    # Load token from cache memory
+    token_info = get_cache_token_info(uid)
+
+    if token_info is None:
+        # Load from firebase
+        print("load token_info from firebase")
+
+        doc_ref = db.collection("users").document(uid)
+        doc = doc_ref.get()
+
+        if not doc.exists:
+            print("not exist")
+            # TODO: show error
+            return Response("not ok")
+
+        token_info = doc.to_dict()
+
+        CACHE_TOKEN_INFO[uid] = token_info
+
+    current_ts = int(time())
+    access_token = token_info["access_token"]
+
+    # Check token expired
+    expired_ts = token_info.get("expired_ts")
+    print(current_ts, expired_ts)
+    if expired_ts is None or current_ts >= expired_ts:
+        # Refresh token
+
+        print("Refresh token")
+
+        refresh_token = token_info["refresh_token"]
+
+        # print(f"refresh_token : {refresh_token}")
+
+        new_token = spotify.refresh_token(refresh_token)
+        expired_ts = int(time()) + new_token["expires_in"]
+        update_data = {"access_token": new_token["access_token"], "expired_ts": expired_ts}
+        doc_ref = db.collection("users").document(uid)
+        doc_ref.update(update_data)
+
+        access_token = new_token["access_token"]
+
+        # Save in memory cache
+        CACHE_TOKEN_INFO[uid] = update_data
+
+    return access_token
+
+
+def get_song_info(uid):
+    access_token = get_access_token(uid)
+
+    data = spotify.get_now_playing(access_token)
+    if data:
+        item = data["item"]
+        item["currently_playing_type"] = data["currently_playing_type"]
+        is_now_playing = True
+    else:
+        recent_plays = spotify.get_recently_play(access_token)
+        size_recent_play = len(recent_plays["items"])
+        idx = random.randint(0, size_recent_play - 1)
+        item = recent_plays["items"][idx]["track"]
+        item["currently_playing_type"] = "track"
+        is_now_playing = False
+
+    return item, is_now_playing
+
+
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def catch_all(path):
+    uid = request.args.get("uid")
+    cover_image = request.args.get("cover_image", default="true") == "true"
+    is_redirect = request.args.get("redirect", default="false") == "true"
+    theme = request.args.get("theme", default="default")
+
+    item, is_now_playing = get_song_info(uid)
+
+    currently_playing_type = item.get("currently_playing_type", "track")
+
+    if is_redirect:
+        return redirect(item["uri"], code=302)
+
+    img = ""
+    if cover_image:
+
+        if currently_playing_type == "track":
+            img = load_image_b64(item["album"]["images"][1]["url"])
+        elif currently_playing_type == "episode":
+            img = load_image_b64(item["images"][1]["url"])
+
+    # Find artist_name and song_name
+    if currently_playing_type == "track":
+        artist_name = item["artists"][0]["name"].replace("&", "&amp;")
+        song_name = item["name"].replace("&", "&amp;")
+
+    elif currently_playing_type == "episode":
+        artist_name = item["show"]["publisher"].replace("&", "&amp;")
+        song_name = item["name"].replace("&", "&amp;")
+
+    svg = make_svg(artist_name, song_name, img, is_now_playing, cover_image, theme)
+
+    resp = Response(svg, mimetype="image/svg+xml")
+    resp.headers["Cache-Control"] = "s-maxage=1"
+
+    print(getsizeof(CACHE_TOKEN_INFO))
+
+    return resp
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5003)
